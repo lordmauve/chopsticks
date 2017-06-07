3ecbccfcc7f818493866da6e856bdd78de4e470c
Daniel Pope <mauve@mauveweb.co.uk>
2017-06-08 00:11:41 +0100

    Add Python3 support for check_output() wrapper

--- chopsticks/helpers.py
+++ chopsticks/helpers.py
@@ -2,21 +2,27 @@ import sys
 import subprocess
 PY2 = sys.version_info < (3,)
 
-try:
-    check_output = subprocess.check_output
-except AttributeError:
-    def check_output(*args, **kwargs):
-        proc = subprocess.Popen(*args, stdout=subprocess.PIPE, **kwargs)
-        stdout, stderr = proc.communicate()
-        if proc.returncode:
-            raise IOError('Subprocess returned %d' % proc.returncode)
-        return stdout
 
+def check_output(*args, **kwargs):
+    try:
+        _check_output = subprocess.check_output
+    except AttributeError:
+        def _check_output(*args, **kwargs):
+            """Polyfill for Python 2.6."""
+            kwargs['stdout'] = subprocess.PIPE
+            p = subprocess.Popen(*args, **kwargs)
+            stdout, stderr = p.communicate()
+            if p.returncode != 0:
+                raise ValueError(
+                    'subprocess exited with return code %s' % p.returncode
+                )
+            return stdout
+    out = _check_output(*args, **kwargs)
+    if not PY2:
+        out = out.decode()
+    return out
 
 
 def output_lines(*args, **kwargs):
     """Return the lines of output from the given command args."""
-    out = check_output(*args, **kwargs)
-    if not PY2:
-        out = out.decode('utf8')
-    return out.splitlines()
+    return check_output(*args, **kwargs).splitlines()
