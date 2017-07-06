9f2f1dc581fad8c8d05e233ef0c883f35dd60f2f
Daniel Pope <mauve@mauveweb.co.uk>
2017-07-06 01:25:00 +0100

    Bump version to 1.0

--- chopsticks/__init__.py
+++ chopsticks/__init__.py
@@ -1,3 +1,6 @@
 import sys
 
+__version__ = '1.0'
+__versioninfo__ = (1, 0)
+
 DEPTH_LIMIT = getattr(sys, '_chopsticks_depthlimit', 2)
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     description='Chopsticks is an orchestration library: it lets you manage ' +
                 'and configure remote hosts over SSH.',
     long_description=open('README.rst').read(),
-    version=0.5,
+    version='1.0',
     author='Daniel Pope',
     author_email='mauve@mauveweb.co.uk',
     url='https://github.com/lordmauve/chopsticks',
