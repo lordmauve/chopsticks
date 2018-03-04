f65f5ffb25e43c13f04163fd4d7b4d7ab4f7a47b
Daniel Pope <mauve@mauveweb.co.uk>
2018-03-04 09:47:42 +0000

    Fix issue with deleting containers

--- clean_unittest_dockers.sh
+++ clean_unittest_dockers.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker ps -a | grep unittest- | sed -e 's/.*\(unittest-[0-9]*\).*/\1/' | xargs -r docker rm
+docker ps -a | grep unittest- | sed -e 's/.*\(unittest-[0-9]*\).*/\1/' | xargs -r docker rm -f
